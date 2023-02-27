d1156b035d28101c3a908425839e26284701b42b
Emilse333 <126480513+Emilse333@users.noreply.github.com>
2023-02-27 10:23:32 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const experience = document.querySelector('.experience');
+const experienceTitle = document.querySelector('.experience-title');
+
+const formation = document.querySelector('.formation');
+const formationTitle = document.querySelector('.formation-title');
+
+const languages = document.querySelector('.languages');
+const languagesTitle = document.querySelector('.languages-title');
+
+function clickHandler(element) {
+  if (element.classList.contains('hidden')) {
+    element.classList.remove('hidden');
+    element.classList.add('visible');
+  } else {
+    element.classList.remove('visible');
+    element.classList.add('hidden');
+  }
+}
+
+experienceTitle.addEventListener('click', () => clickHandler(experience));
+formationTitle.addEventListener('click', () => clickHandler(formation));
+languagesTitle.addEventListener('click', () => clickHandler(languagesTitle));
