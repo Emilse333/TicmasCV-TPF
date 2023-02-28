7bd15d9bc4302df3e2524e2aa30faa26d9c31206
Emilse333 <126480513+Emilse333@users.noreply.github.com>
2023-02-28 09:03:29 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 Copy code// Crea un nuevo elemento de imagen 
 var img = document.createElement('img'); 
- 
-var img = document.getElementById("https://static.wikia.nocookie.net/lossimpson/images/8/82/John.png/revision/latest?cb=20090901202749&path-prefix=es");
-
+var img = document.getElementById("ID_del_div").innerHTML=("https://static.wikia.nocookie.net/lossimpson/images/8/82/John.png/revision/latest?cb=20090901202749&path-prefix=es"); />';
 const experience = document.querySelector('.experience');
 const experienceTitle = document.querySelector('.experience-title');
 
